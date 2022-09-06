251f1933a1a3cae6493635f624cab4460bdb672e
Eric Blaney <EricBlaney@gmail.com>
2022-09-06 19:48:31 -0400

    Added timer attached to ball release

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import sky from './assets/skyRuler.png';
 import ground from './assets/Ground.png';
 import ball from './assets/ball.png';
 
+
 const config = {
     type: Phaser.AUTO,
     width: 800,
@@ -26,10 +27,12 @@ function preload() {
     this.load.image('sky', sky);
     this.load.image('ground', ground);
     this.load.image('ball', ball);
+
 }
 
-let platforms;
-let player;
+let text;
+let timedEvent;
+
 function create() {
     this.add.image(400, 300, 'sky');
  
@@ -37,11 +40,37 @@ function create() {
 
     const canDrag = this.matter.world.nextGroup();
 
-    this.matter.add.image(48, 46, 'ball', null, { chamfer: 16 }).setBounce(0.7).setCollisionGroup(canDrag);
-    this.matter.add.image(400, 599, 'ground').setScale(3).setStatic(true);
+    const ball1 = this.matter.add.image(48, 46, 'ball', null, { chamfer: 16 }).setBounce(0.7).setCollisionGroup(canDrag);
+    const ground1 = this.matter.add.image(400, 600, 'ground').setScale(3).setStatic(true);
     this.matter.add.mouseSpring({ length: 1, stiffness: 0.6, collisionFilter: { group: canDrag } });
+
+    
+    text = this.add.text(400,32)
+    timedEvent = this.time.addEvent({ delay: 5000, timeScale: 0.93 });
+
+
+    ball1.setInteractive();
+
+    ball1.on('pointerup', function(){
+        timedEvent = this.time.addEvent({
+            delay: 5000,
+            timeScale: 0.93
+        })
+    },this)
+
+    this.matter.world.on('collisionstart', function (event, ball1, ground1) {
+        timedEvent['paused'] = true ;
+        console.log('hit');
+    })
+
+
+
 }
 
+
+
 function update() {
+    let progress = timedEvent.getProgress() * 5;
+    text.setText('Fall time: ' + progress.toString().substr(0, 4) + 's');
 
 }
